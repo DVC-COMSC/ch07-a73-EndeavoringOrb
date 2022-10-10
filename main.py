9412fbc61b5eaaaaffe24cd7088191795d5af3ca
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-10 14:50:21 -0700

    2nd commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ numbers = [5, 20, 30, 35, 50]
 insval = int(input('Enter the insertion value: '))
 
 for i in range(len(numbers)):
-    if insval > numbers:
+    if insval > len(numbers):
         continue
     else:
         numbers.insert(i,insval)
